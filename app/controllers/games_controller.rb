79c66739bf94ee590003e1637ad0393083e40ca8
Anastasia Parfait <anastasia.parfait@audencia.com>
2021-11-15 17:34:27 +0100

    Not finished if word is valid Longest Word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,26 @@
+class GamesController < ApplicationController
+  def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @word = params[:word]
+    @include = @word.chars.all? { |letter| params[:word].count(letter) <= @word.count(letter) }
+    p @include
+    if @include
+      if english_word
+        'well done'
+      else
+        'not an english word'
+      end
+    else
+      'not in the grid'
+    end
+  end
+
+  def english_word
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
